679abce8b9088df5027919b337c95ff4ac90a9df
Jake Kile <jkile9@gmail.com>
2020-04-01 21:49:35 -0700

    fixed service working and db.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,13 @@
 const FILES_TO_CACHE = [
     "/",
-    "index.html"
+    "/index.html",
+    "/index.js",
+    "/styles.css"
 ];
 
 const PRECACHE = "precache-v1";
 const RUNTIME = "runtime";
+const DATA_CACHE_NAME = "budgetCache"
 
 self.addEventListener("install", event => {
     event.waitUntil(
@@ -27,22 +30,35 @@ self.addEventListener("activate", event => {
     );
 });
 
-self.addEventListener("fetch", event => {
-    if (event.request.url.startsWith(self.location.origin)) {
-        event.respondWith(
-            caches.match(event.request).then(cachedResponse => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
+self.addEventListener("fetch", function (evt) {
+    // cache successful requests to the API
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
 
-                return caches.open(RUNTIME).then(cache => {
-                    return fetch(event.request).then(response => {
-                        return cache.put(event.request, response.clone()).then(() => {
-                            return response;
-                        });
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
                     });
-                });
-            })
+            }).catch(err => console.log(err))
         );
+
+        return;
     }
+
+    // if the request is not for the API, serve static assets using "offline-first" approach.
+    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+    evt.respondWith(
+        caches.match(evt.request).then(function (response) {
+            return response || fetch(evt.request);
+        })
+    );
 });
